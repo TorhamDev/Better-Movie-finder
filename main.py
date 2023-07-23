1a2ac775d56191d725e31a6a2868ca60f3e73f03
Torham <torhamdev@gmail.com>
2023-07-23 17:46:05 +0330

    Add css and xpath selector

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.scraper.Scraper import Scraper
+
+scraper = Scraper("https://avamovie2.info/?s=spider+man")
+
+print(scraper.scrap())
--- src/scraper/Scraper.py
+++ src/scraper/Scraper.py
@@ -1,9 +1,11 @@
 import requests  # type: ignore
+from scrapy.selector import Selector  # type: ignore
 
 
 class Scraper:
     def __init__(self, url) -> None:
         self.url = url
+        self.request_content = None
 
     def scrap(self):
         req = requests.get(url=self.url)
@@ -12,3 +14,9 @@ class Scraper:
             req.content
 
         raise ValueError(f"Recived {req.status_code} http status code from {self.url}")
+
+    def css(self, css_query: str):
+        return Selector(text=self.request_content).css(css_query)
+
+    def xpath(self, xpath_query: str):
+        return Selector(text=self.request_content).xpath(xpath_query)
