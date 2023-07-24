5cb5491294b58c1e5c64b2f1553ed4fe77e19f22
Torham <torhamdev@gmail.com>
2023-07-24 15:59:00 +0330

    Add simple menu with inquirer

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
-from spiders.search import SearchSpider
+import inquirer
 
-spider = SearchSpider()
-data = spider.search("amazing spider man")
+options = ["🔎 Search movie name", "⬇️ Get a movie download links", "📡 Both"]
 
-for i in data:
-    print(i.css("h2.title a::attr(href)").extract()[0])
-    print(i.css("h2.title ::text").extract()[0])
+questions = [
+    inquirer.List(
+        "option",
+        message="Select menu",
+        choices=options,
+    ),
+]
+answers = inquirer.prompt(questions)
+
+print(answers)
+print(answers["option"])
+
+
+# from spiders.search import SearchSpider
+
+# spider = SearchSpider()
+# data = spider.search("spider man")
+
+# for i in data:
+#     print(i.css("h2.title a::attr(href)").extract()[0])
+#     print(i.css("h2.title ::text").extract()[0])
