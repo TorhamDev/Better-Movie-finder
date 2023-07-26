5d936cbd6c544f09434de82b40ec945affd9879c
Mahdi <d88560771@gmail.com>
2023-07-26 23:39:54 +0330

    Add Gui app for searching

--- src/MovieFinder.py
+++ src/MovieFinder.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtWidgets import QWidget, QApplication, QMessageBox
 from MainWindow.MainWindowUI import Ui_Form
+from spiders.download_links import DownloadLinksSpider  # type: ignore
+from spiders.search import SearchSpider
 
 
 class Window(QWidget, Ui_Form):
@@ -8,6 +10,44 @@ class Window(QWidget, Ui_Form):
         super().__init__()
         self.setupUi(self)
         self.show()
+        self.lineEdit.returnPressed.connect(self.search_signal)
+        self.search_btn.clicked.connect(self.search_signal)
+        self.list_widget.clicked.connect(self.select_movie)
+        self.clear_btn.clicked.connect(self.clear)
+
+
+
+
+
+
+
+
+
+
+    def search_signal(self):
+        if self.lineEdit.text() == '':
+            QMessageBox.warning(self, 'Warning', 'You didn\'t type anything !',
+                                QMessageBox.StandardButton.Ok)
+        else :
+                spider = SearchSpider()
+                self.search_results = spider.search(query=self.lineEdit.text())
+                items = [k for k, v in self.search_results.items()]
+                self.list_widget.addItems(items)
+        
+
+    def select_movie(self):
+        name = self.list_widget.currentItem().text()
+        text = 'Movie Links :\n\n'
+        spider = DownloadLinksSpider(self.search_results[name])
+        results = spider.get_download_links()
+        for quality, link in results:
+            text += f"{quality} :\n{link}\n----------------\n"
+        self.output.setText(text)
+
+    def clear(self):
+         self.list_widget.clear()
+         self.output.clear()
+         self.search_results = {}
 
 
 
