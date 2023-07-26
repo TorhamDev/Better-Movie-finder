9957d43b50e808d6a112228138c9f8d2e31305fc
Torham <torhamdev@gmail.com>
2023-07-26 17:53:36 +0330

    Add some emoji :sparkles:

--- src/main.py
+++ src/main.py
@@ -1,18 +1,25 @@
 from spiders.download_links import DownloadLinksSpider  # type: ignore
 from spiders.search import SearchSpider  # type: ignore
 
-from utils.banner import print_banner  # type: ignore
+from utils.banner import print_banner, columns as terminal_columns  # type: ignore
 from utils.tools import ask_with_options, clear_terminal_screen  # type: ignore
 import settings  # type: ignore
 from rich import print
+from rich.prompt import Prompt
 
 
 def search_movie():
-    query = input("Enter movie name: ")
+    query = Prompt.ask(":movie_camera: Enter movie name")
     spider = SearchSpider()
     search_results = spider.search(query=query)
     clear_terminal_screen()
-    print("Result Count: ", len(search_results))
+    print(
+        "\n",
+        f":sparkles: :cake: [red]Result Count:[/red] {len(search_results)}:sparkles:".center(
+            terminal_columns
+        ),
+        "\n",
+    )
     seleceted = ask_with_options(
         options=search_results.keys(),
         question=settings.SEARCH_SELECET_QUESTION,
@@ -22,7 +29,7 @@ def search_movie():
 
 def get_moive_download_links(page_url=None):
     if not page_url:
-        page_url = input("Enter movie page url: ")
+        page_url = Prompt.ask(":movie_camera: Enter movie page url")
 
     spider = DownloadLinksSpider(page_url)
     results = spider.get_download_links()
@@ -41,7 +48,7 @@ def main():
 
     elif user_choice == settings.USER_CHOICE_SEARCH_MOIVE:
         result = search_movie()
-        print("Movie Page link: ", result)
+        print(":smiling_imp: Movie Page link: ", result)
 
     elif user_choice == settings.USER_CHOICE_GET_DOWNLOAD_LINK:
         get_moive_download_links()
